914d0fde011f2f9c869f9a297e9b6cbe7b14d7b1
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2023-11-19 16:46:09 +0100

    Add items to assets

--- lib/static/resources/assets.dart
+++ lib/static/resources/assets.dart
@@ -47,4 +47,73 @@ final class Assets {
   static const challenger2 = Asset('assets/divisions/challenger/2.png');
   static const challenger3 = Asset('assets/divisions/challenger/3.png');
   static const challenger4 = Asset('assets/divisions/bronze/4.png');
+
+  static const bfSword = Asset('assets/items/base/bf_sword.png');
+  static const chainVest = Asset('assets/items/base/chain_vest.png');
+  static const giantsBelt = Asset('assets/items/base/giants_belt.png');
+  static const needlesslyLargeRod =
+      Asset('assets/items/base/needlessly_large_rod.png');
+  static const negatronCloak = Asset('assets/items/base/negatron_cloak.png');
+  static const recurveBow = Asset('assets/items/base/recurve_bow.png');
+  static const sparringGloves = Asset('assets/items/base/sparring_gloves.png');
+  static const spatula = Asset('assets/items/base/spatula.png');
+  static const tearOfTheGoddess =
+      Asset('assets/items/base/tear_of_the_goddess.png');
+
+  static const adaptiveHelm = Asset('assets/items/full/adaptive_helm.png');
+  static const archangelsStaff =
+      Asset('assets/items/full/archangels_staff.png');
+  static const bloodthirster = Asset('assets/items/full/bloodthirster.png');
+  static const blueBuff = Asset('assets/items/full/blue_buff.png');
+  static const brambleVest = Asset('assets/items/full/bramble_vest.png');
+  static const chaliceOfPower = Asset('assets/items/full/chalice_of_power.png');
+  static const crownguard = Asset('assets/items/full/crownguard.png');
+  static const deathblade = Asset('assets/items/full/deathblade.png');
+  static const dragonsClaw = Asset('assets/items/full/dragons_claw.png');
+  static const edgeOfNight = Asset('assets/items/full/edge_of_night.png');
+  static const evenshroud = Asset('assets/items/full/evenshroud.png');
+  static const gargoyleStoneplate =
+      Asset('assets/items/full/gargoyle_stoneplate.png');
+  static const giantSlayer = Asset('assets/items/full/giant_slayer.png');
+  static const guardbreaker = Asset('assets/items/full/guardbreaker.png');
+  static const guinsoosRageblade =
+      Asset('assets/items/full/guinsoos_rageblade.png');
+  static const handOfJustice = Asset('assets/items/full/hand_of_justice.png');
+  static const hextechGunblade =
+      Asset('assets/items/full/hextech_gunblade.png');
+  static const infinityEdge = Asset('assets/items/full/infinity_edge.png');
+  static const ionicSpark = Asset('assets/items/full/ionic_spark.png');
+  static const jeweledGauntlet =
+      Asset('assets/items/full/jeweled_gauntlet.png');
+  static const lastWhisper = Asset('assets/items/full/last_whisper.png');
+  static const locketOfTheIronSolari =
+      Asset('assets/items/full/locket_of_the_iron_solari.png');
+  static const morellonomicon = Asset('assets/items/full/morellonomicon.png');
+  static const nashorsTooth = Asset('assets/items/full/nashors_tooth.png');
+  static const nightHarvester = Asset('assets/items/full/night_harvester.png');
+  static const protectorsVow = Asset('assets/items/full/protectors_vow.png');
+  static const quicksilver = Asset('assets/items/full/quicksilver.png');
+  static const rabadonsDeathcap =
+      Asset('assets/items/full/rabadons_deathcap.png');
+  static const rapidFirecannon =
+      Asset('assets/items/full/rapid_firecannon.png');
+  static const redBuff = Asset('assets/items/full/red_buff.png');
+  static const redemption = Asset('assets/items/full/redemption.png');
+  static const runaansHurricane =
+      Asset('assets/items/full/runaans_hurricane.png');
+  static const shroudOfStillness =
+      Asset('assets/items/full/shroud_of_stillness.png');
+  static const spearOfShojin = Asset('assets/items/full/spear_of_shojin.png');
+  static const statikkShiv = Asset('assets/items/full/statikk_shiv.png');
+  static const steadfastHeart = Asset('assets/items/full/steadfast_heart.png');
+  static const steraksGage = Asset('assets/items/full/steraks_gage.png');
+  static const sunfireCape = Asset('assets/items/full/sunfire_cape.png');
+  static const tacticiansCrown =
+      Asset('assets/items/full/tacticians_crown.png');
+  static const thiefsGloves = Asset('assets/items/full/thiefs_gloves.png');
+  static const titansResolve = Asset('assets/items/full/titans_resolve.png');
+  static const warmogsArmor = Asset('assets/items/full/warmogs_armor.png');
+  static const zekesHerald = Asset('assets/items/full/zekes_herald.png');
+  static const zephyr = Asset('assets/items/full/zephyr.png');
+  static const zzRotPortal = Asset('assets/items/full/zz_rot_portal.png');
 }
