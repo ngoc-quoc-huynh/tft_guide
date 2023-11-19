d5a347f6efa676589c649a54e34ee295405c9d36
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2023-11-19 16:14:21 +0100

    Add assets for divisions

--- lib/static/resources/assets.dart
+++ lib/static/resources/assets.dart
@@ -6,4 +6,45 @@ final class Asset {
 
 final class Assets {
   const Assets._();
+
+  static const iron1 = Asset('assets/divisions/iron/1.png');
+  static const iron2 = Asset('assets/divisions/iron/2.png');
+  static const iron3 = Asset('assets/divisions/iron/3.png');
+  static const iron4 = Asset('assets/divisions/iron/4.png');
+  static const bronze1 = Asset('assets/divisions/bronze/1.png');
+  static const bronze2 = Asset('assets/divisions/bronze/2.png');
+  static const bronze3 = Asset('assets/divisions/bronze/3.png');
+  static const bronze4 = Asset('assets/divisions/bronze/4.png');
+  static const silver1 = Asset('assets/divisions/silver/1.png');
+  static const silver2 = Asset('assets/divisions/silver/2.png');
+  static const silver3 = Asset('assets/divisions/silver/3.png');
+  static const silver4 = Asset('assets/divisions/silver/4.png');
+  static const gold1 = Asset('assets/divisions/gold/1.png');
+  static const gold2 = Asset('assets/divisions/gold/2.png');
+  static const gold3 = Asset('assets/divisions/gold/3.png');
+  static const gold4 = Asset('assets/divisions/gold/4.png');
+  static const platinum1 = Asset('assets/divisions/platinum/1.png');
+  static const platinum2 = Asset('assets/divisions/platinum/2.png');
+  static const platinum3 = Asset('assets/divisions/platinum/3.png');
+  static const platinum4 = Asset('assets/divisions/platinum/4.png');
+  static const emerald1 = Asset('assets/divisions/emerald/1.png');
+  static const emerald2 = Asset('assets/divisions/emerald/2.png');
+  static const emerald3 = Asset('assets/divisions/emerald/3.png');
+  static const emerald4 = Asset('assets/divisions/emerald/4.png');
+  static const diamond1 = Asset('assets/divisions/diamond/1.png');
+  static const diamond2 = Asset('assets/divisions/diamond/2.png');
+  static const diamond3 = Asset('assets/divisions/diamond/3.png');
+  static const diamond4 = Asset('assets/divisions/diamond/4.png');
+  static const master1 = Asset('assets/divisions/master/1.png');
+  static const master2 = Asset('assets/divisions/master/2.png');
+  static const master3 = Asset('assets/divisions/master/3.png');
+  static const master4 = Asset('assets/divisions/master/4.png');
+  static const grandmaster1 = Asset('assets/divisions/grandmaster/1.png');
+  static const grandmaster2 = Asset('assets/divisions/grandmaster/2.png');
+  static const grandmaster3 = Asset('assets/divisions/grandmaster/3.png');
+  static const grandmaster4 = Asset('assets/divisions/grandmaster/4.png');
+  static const challenger1 = Asset('assets/divisions/challenger/1.png');
+  static const challenger2 = Asset('assets/divisions/challenger/2.png');
+  static const challenger3 = Asset('assets/divisions/challenger/3.png');
+  static const challenger4 = Asset('assets/divisions/bronze/4.png');
 }
