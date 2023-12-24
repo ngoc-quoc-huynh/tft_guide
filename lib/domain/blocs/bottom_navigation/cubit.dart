fc3100e584679952a6afee17d27a550f44832376
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2023-12-24 15:27:27 +0100

    Replace BottomNavigation from Bloc to GoRouter

--- lib/domain/blocs/bottom_navigation/cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-part 'state.dart';
-
-final class BottomNavigationCubit extends Cubit<BottomNavigationState> {
-  BottomNavigationCubit() : super(BottomNavigationState.ranked);
-
-  void switchStateTo(BottomNavigationState state) => emit(state);
-}
