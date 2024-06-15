6b0618e7c2d0bc6e133eaa116a575027ff5bb8de
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-06-15 14:50:08 +0200

    Make item attributes optional

--- lib/domain/models/item.dart
+++ lib/domain/models/item.dart
@@ -6,30 +6,30 @@ sealed class Item extends Equatable {
     required this.name,
     required this.description,
     required this.asset,
-    required this.abilityPower,
-    required this.armor,
-    required this.attackDamage,
-    required this.attackSpeed,
-    required this.crit,
-    required this.health,
-    required this.magicResist,
-    required this.mana,
+    this.abilityPower,
+    this.armor,
+    this.attackDamage,
+    this.attackSpeed,
+    this.crit,
+    this.health,
+    this.magicResist,
+    this.mana,
   });
 
   final String name;
   final String description;
   final Asset asset;
-  final int abilityPower;
-  final int armor;
-  final int attackDamage;
-  final int attackSpeed;
-  final int crit;
-  final int health;
-  final int magicResist;
-  final int mana;
+  final int? abilityPower;
+  final int? armor;
+  final int? attackDamage;
+  final int? attackSpeed;
+  final int? crit;
+  final int? health;
+  final int? magicResist;
+  final int? mana;
 
   @override
-  List<Object> get props => [
+  List<Object?> get props => [
         name,
         description,
         asset,
@@ -49,14 +49,14 @@ final class BaseItem extends Item {
     required super.name,
     required super.description,
     required super.asset,
-    required super.abilityPower,
-    required super.armor,
-    required super.attackDamage,
-    required super.attackSpeed,
-    required super.crit,
-    required super.health,
-    required super.magicResist,
-    required super.mana,
+    super.abilityPower,
+    super.armor,
+    super.attackDamage,
+    super.attackSpeed,
+    super.crit,
+    super.health,
+    super.magicResist,
+    super.mana,
   });
 }
 
@@ -65,23 +65,23 @@ final class FullItem extends Item {
     required super.name,
     required super.description,
     required super.asset,
-    required super.abilityPower,
-    required super.armor,
-    required super.attackDamage,
-    required super.attackSpeed,
-    required super.crit,
-    required super.health,
-    required super.magicResist,
-    required super.mana,
     required this.baseItem1,
     required this.baseItem2,
+    super.abilityPower,
+    super.armor,
+    super.attackDamage,
+    super.attackSpeed,
+    super.crit,
+    super.health,
+    super.magicResist,
+    super.mana,
   });
 
   final BaseItem baseItem1;
   final BaseItem baseItem2;
 
   @override
-  List<Object> get props => [
+  List<Object?> get props => [
         ...super.props,
         baseItem1,
         baseItem2,
