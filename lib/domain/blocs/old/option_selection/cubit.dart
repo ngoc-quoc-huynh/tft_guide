75c613a0fe288ef56bc3c875b69a85c410418889
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-07-13 13:20:59 +0200

    Remove legacy code

--- lib/domain/blocs/old/option_selection/cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tft_guide/domain/models/old/item.dart';
-
-// TODO: Maybe refactor to a value cubit
-final class OptionSelectionCubit extends Cubit<Item?> {
-  OptionSelectionCubit() : super(null);
-
-  void select(Item newOption) => emit(newOption);
-}
--- lib/domain/blocs/old/show_correct_option/cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-// TODO: Maybe refactor to a value cubit
-final class ShowCorrectOptionCubit extends Cubit<bool> {
-  ShowCorrectOptionCubit() : super(false);
-
-  void show() => emit(true);
-}
