0fddc45703bc9af1cf69569171c3eb4b85f20d82
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-08-19 10:52:32 +0200

    Add storing of items

--- app/lib/domain/interfaces/local_database.dart
+++ app/lib/domain/interfaces/local_database.dart
@@ -1,3 +1,5 @@
+import 'package:tft_guide/infrastructure/models/sqlite_async/item.dart';
+
 abstract interface class LocalDatabaseAPI {
   const LocalDatabaseAPI();
 
@@ -10,6 +12,10 @@ abstract interface class LocalDatabaseAPI {
   Future<DateTime?> loadLatestBaseItemTranslationUpdatedAt();
 
   Future<DateTime?> loadLatestFullItemTranslationUpdatedAt();
+
+  Future<void> storeBaseItems(List<BaseItem> items);
+
+  Future<void> storeFullItems(List<FullItem> items);
 
   Future<void> close();
 }
