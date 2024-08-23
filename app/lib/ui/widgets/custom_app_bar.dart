f0a69bf3fe5afa5927cd1dde1ca975252ae2e664
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-08-23 11:51:27 +0200

    Add custom font and icons

--- app/lib/ui/widgets/custom_app_bar.dart
+++ app/lib/ui/widgets/custom_app_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:tft_guide/static/resources/assets.dart';
+import 'package:tft_guide/static/resources/icons.dart';
 
 class CustomAppBar extends AppBar {
   CustomAppBar({
@@ -8,9 +7,38 @@ class CustomAppBar extends AppBar {
     super.key,
   }) : super(
           centerTitle: true,
-          title: SvgPicture.asset(
-            Assets.logo.path,
-            height: kToolbarHeight,
-          ),
+          title: const _Title(),
         );
+}
+
+class _Title extends StatelessWidget {
+  const _Title();
+
+  // TODO: Harmonize color
+  static const gradient = LinearGradient(
+    colors: [
+      Color(0xFFF8B33F),
+      Color(0xFFD78C3A),
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: Text(
+        String.fromCharCode(TftIcons.logo.codePoint),
+        style: const TextStyle(
+          fontFamily: TftIcons.fontFamily,
+          fontSize: kToolbarHeight,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
 }
