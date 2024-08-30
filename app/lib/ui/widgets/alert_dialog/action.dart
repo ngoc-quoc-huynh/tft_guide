33f7634325f8327f355ba2cb0e50f97cd1cad09b
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-08-30 10:55:41 +0200

    Add CustomDialog

---
+++ app/lib/ui/widgets/alert_dialog/action.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tft_guide/injector.dart';
+import 'package:tft_guide/static/i18n/translations.g.dart';
+
+sealed class AlertDialogAction extends StatelessWidget {
+  const AlertDialogAction({
+    super.key,
+  });
+
+  const factory AlertDialogAction.cancel({
+    VoidCallback? onPressed,
+    Key? key,
+  }) = _Cancel;
+
+  const factory AlertDialogAction.confirm({
+    required VoidCallback onPressed,
+    Key? key,
+  }) = _Confirm;
+
+  const factory AlertDialogAction.custom({
+    required String text,
+    required VoidCallback onPressed,
+    Key? key,
+  }) = _Custom;
+
+  @protected
+  TranslationsGeneralDe get translations =>
+      Injector.instance.translations.general;
+}
+
+final class _Cancel extends AlertDialogAction {
+  const _Cancel({
+    this.onPressed,
+    super.key,
+  });
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed ?? () => context.pop(),
+      child: Text(translations.cancel),
+    );
+  }
+}
+
+final class _Confirm extends AlertDialogAction {
+  const _Confirm({
+    required this.onPressed,
+    super.key,
+  });
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Text(translations.confirm),
+    );
+  }
+}
+
+final class _Custom extends AlertDialogAction {
+  const _Custom({
+    required this.text,
+    required this.onPressed,
+    super.key,
+  });
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
---
+++ app/lib/ui/widgets/alert_dialog/dialog.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:tft_guide/static/resources/sizes.dart';
+import 'package:tft_guide/ui/widgets/alert_dialog/action.dart';
+
+class CustomAlertDialog extends StatelessWidget {
+  const CustomAlertDialog({
+    required this.title,
+    required this.content,
+    required this.actions,
+    super.key,
+  });
+
+  final String title;
+  final Widget content;
+  final List<AlertDialogAction> actions;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: _shape,
+      titlePadding: _titlePadding,
+      title: Text(title),
+      contentPadding: _contentPadding,
+      content: content,
+      actionsPadding: _actionsPadding,
+      actions: actions,
+    );
+  }
+
+  static const _titlePadding = EdgeInsets.only(
+    top: Sizes.verticalPadding,
+    left: Sizes.horizontalPadding,
+    right: Sizes.horizontalPadding,
+  );
+  static const _contentPadding = EdgeInsets.only(
+    top: Sizes.verticalPadding,
+    bottom: Sizes.verticalPadding / 2,
+  );
+  static const _actionsPadding = EdgeInsets.only(
+    left: Sizes.horizontalPadding,
+    right: Sizes.horizontalPadding,
+    bottom: Sizes.verticalPadding / 2,
+  );
+  static const _shape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(20)),
+  );
+}
