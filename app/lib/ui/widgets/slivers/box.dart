3a5e4309bf8a35a483bd79064a151c5c1fb89ac6
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-04 15:33:16 +0200

    Convert SliverSizedBox to a real sliver

--- app/lib/ui/widgets/slivers/box.dart
+++ app/lib/ui/widgets/slivers/box.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
-// TODO: Create a real sliver
-class SliverSizedBox extends StatelessWidget {
+class SliverSizedBox extends SingleChildRenderObjectWidget {
   const SliverSizedBox({
     this.width,
     this.height,
@@ -12,12 +12,60 @@ class SliverSizedBox extends StatelessWidget {
   final double? height;
 
   @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: SizedBox(
+  RenderSliverSizedBox createRenderObject(BuildContext context) =>
+      RenderSliverSizedBox(
         width: width,
         height: height,
-      ),
+      );
+
+  @override
+  void updateRenderObject(
+    BuildContext context,
+    RenderSliverSizedBox renderObject,
+  ) =>
+      renderObject
+        ..width = width
+        ..height = height;
+}
+
+class RenderSliverSizedBox extends RenderSliver {
+  RenderSliverSizedBox({
+    double? width,
+    double? height,
+  })  : _width = width,
+        _height = height;
+
+  double? _width;
+  double? _height;
+
+  double? get width => _width;
+
+  double? get height => _height;
+
+  set width(double? value) {
+    if (_width != value) {
+      _width = value;
+      markNeedsLayout();
+    }
+  }
+
+  set height(double? value) {
+    if (_height != value) {
+      _height = value;
+      markNeedsLayout();
+    }
+  }
+
+  @override
+  void performLayout() {
+    final paintExtent = height ?? constraints.remainingPaintExtent;
+    final crossAxisExtent = width ?? constraints.crossAxisExtent;
+
+    geometry = SliverGeometry(
+      scrollExtent: paintExtent,
+      paintExtent: paintExtent.clamp(0, constraints.remainingPaintExtent),
+      maxPaintExtent: paintExtent,
+      crossAxisExtent: crossAxisExtent,
     );
   }
 }
