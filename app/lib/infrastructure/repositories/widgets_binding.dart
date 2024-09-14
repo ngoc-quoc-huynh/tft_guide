c76691af50e17865decfb35c6dcd60376fa2a05b
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-14 17:53:07 +0200

    Add logging to WidgetsBindingRepository

--- app/lib/infrastructure/repositories/widgets_binding.dart
+++ app/lib/infrastructure/repositories/widgets_binding.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tft_guide/domain/interfaces/widgets_binding.dart';
+import 'package:tft_guide/domain/utils/mixins/logger.dart';
 
-final class WidgetsBindingRepository implements WidgetsBindingApi {
+final class WidgetsBindingRepository
+    with LoggerMixin
+    implements WidgetsBindingApi {
   const WidgetsBindingRepository();
 
   @override
-  Brightness get brightness =>
-      WidgetsBinding.instance.platformDispatcher.platformBrightness;
+  Brightness get brightness {
+    final brightness =
+        WidgetsBinding.instance.platformDispatcher.platformBrightness;
+    logInfo(
+      'WidgetsBindingRepository.brightness',
+      'Retrieved brightness: $brightness.',
+      stackTrace: StackTrace.current,
+    );
+
+    return brightness;
+  }
 
   @override
-  Locale get locale => WidgetsBinding.instance.platformDispatcher.locale;
+  Locale get locale {
+    final locale = WidgetsBinding.instance.platformDispatcher.locale;
+    logInfo(
+      'WidgetsBindingRepository.locale',
+      'Retrieved locale: $locale',
+      stackTrace: StackTrace.current,
+    );
+
+    return locale;
+  }
 }
