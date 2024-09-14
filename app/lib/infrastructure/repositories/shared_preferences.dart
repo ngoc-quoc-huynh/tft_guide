b0ff93bdfa84944d9896e5671ceb9b722e69001b
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-14 17:51:29 +0200

    Add logging to SharedPreferencesRepository

--- app/lib/infrastructure/repositories/shared_preferences.dart
+++ app/lib/infrastructure/repositories/shared_preferences.dart
@@ -1,28 +1,64 @@
 import 'package:tft_guide/domain/interfaces/local_storage.dart';
 import 'package:tft_guide/domain/utils/extensions/date_time.dart';
+import 'package:tft_guide/domain/utils/mixins/logger.dart';
 import 'package:tft_guide/injector.dart';
 
-final class SharedPreferencesRepository implements LocalStorageApi {
+final class SharedPreferencesRepository
+    with LoggerMixin
+    implements LocalStorageApi {
   static final _sharedPrefs = Injector.instance.sharedPrefs;
 
   @override
-  DateTime? get lastAppUpdate => DateTimeExtension.tryParseOrNull(
-        _sharedPrefs.getString(_lastAppUpdateKey),
-      );
+  DateTime? get lastAppUpdate {
+    final lastUpdate = DateTimeExtension.tryParseOrNull(
+      _sharedPrefs.getString(_lastAppUpdateKey),
+    );
+    logInfo(
+      'SharedPreferencesRepository.lastAppUpdate',
+      'Retrieved last app update: ${lastUpdate?.toIso8601String()}.',
+      stackTrace: StackTrace.current,
+    );
+
+    return lastUpdate;
+  }
 
   @override
-  Future<void> updateLastAppUpdate(DateTime date) => _sharedPrefs.setString(
-        _lastAppUpdateKey,
-        date.toUtc().toIso8601String(),
-      );
+  Future<void> updateLastAppUpdate(DateTime date) async {
+    await _sharedPrefs.setString(
+      _lastAppUpdateKey,
+      date.toUtc().toIso8601String(),
+    );
+
+    logInfo(
+      'SharedPreferencesRepository.updateLastAppUpdate',
+      'Updated last app update.',
+      stackTrace: StackTrace.current,
+    );
+  }
 
   @override
-  int get readPatchNotesCount =>
-      _sharedPrefs.getInt(_readPatchNotesCountKey) ?? 0;
+  int get readPatchNotesCount {
+    final count = _sharedPrefs.getInt(_readPatchNotesCountKey) ?? 0;
+    logInfo(
+      'SharedPreferencesRepository.readPatchNotesCount',
+      'Retrieved read patch notes count: $count',
+      stackTrace: StackTrace.current,
+    );
+
+    return count;
+  }
 
   @override
-  Future<void> updateReadPatchNotesCount(int count) =>
-      _sharedPrefs.setInt(_readPatchNotesCountKey, count);
+  Future<void> updateReadPatchNotesCount(int count) async {
+    await _sharedPrefs.setInt(_readPatchNotesCountKey, count);
+
+    logInfo(
+      'SharedPreferencesRepository.updateReadPatchNotesCount',
+      'Updated read patch notes count to $count.',
+      stackTrace: StackTrace.current,
+      parameters: {'count': count.toString()},
+    );
+  }
 
   static const _lastAppUpdateKey = 'last_app_update';
   static const _readPatchNotesCountKey = 'read_patch_notes_count';
