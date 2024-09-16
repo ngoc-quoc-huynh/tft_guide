d8da353ba8a305b63ac0b34cbe90a21cfc728a3d
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-16 13:12:13 +0200

    Remove registering of license, since providing a LICENSE file will add it

--- app/lib/ui/pages/license/page.dart
+++ app/lib/ui/pages/license/page.dart
@@ -1,29 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tft_guide/injector.dart';
 import 'package:tft_guide/static/i18n/translations.g.dart';
 import 'package:tft_guide/static/resources/assets.dart';
 
-class CustomLicensePage extends StatefulWidget {
+class CustomLicensePage extends StatelessWidget {
   const CustomLicensePage({super.key});
-
-  @override
-  State<CustomLicensePage> createState() => _CustomLicensePageState();
-}
-
-class _CustomLicensePageState extends State<CustomLicensePage> {
-  @override
-  void initState() {
-    super.initState();
-    LicenseRegistry.addLicense(
-      () async* {
-        yield LicenseEntryWithLineBreaks(
-          [_translations.appName],
-          _licensePageTranslation.license,
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
