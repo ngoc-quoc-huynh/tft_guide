3c7f6b34762f8191acdd12c0cb2558b6118b4913
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-18 15:14:41 +0200

    Add restart app after repair

--- app/lib/ui/pages/settings/dialogs/repair/button.dart
+++ app/lib/ui/pages/settings/dialogs/repair/button.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:tft_guide/domain/blocs/repair/bloc.dart';
 import 'package:tft_guide/injector.dart';
 import 'package:tft_guide/static/i18n/translations.g.dart';
@@ -10,19 +11,22 @@ class SettingsRepairStartButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<RepairBloc, RepairState>(
-      builder: (context, state) => FilledButton(
-        onPressed: _onPressed(context, state),
-        child: switch (state) {
-          RepairInitial() ||
-          RepairLoadInProgress() ||
-          RepairAnimationInProgress() =>
-            Text(_translations.start),
-          RepairLoadOnFailure() => Text(_translations.retry),
-          RepairAnimationInProgress() ||
-          RepairLoadOnSuccess() =>
-            const Icon(Icons.check),
-        },
+    return PopScope(
+      onPopInvokedWithResult: (_, __) => _onPop(context),
+      child: BlocBuilder<RepairBloc, RepairState>(
+        builder: (context, state) => FilledButton(
+          onPressed: _onPressed(context, state),
+          child: switch (state) {
+            RepairInitial() ||
+            RepairLoadInProgress() ||
+            RepairAnimationInProgress() =>
+              Text(_translations.start),
+            RepairLoadOnFailure() => Text(_translations.retry),
+            RepairAnimationInProgress() ||
+            RepairLoadOnSuccess() =>
+              const Icon(Icons.check),
+          },
+        ),
       ),
     );
   }
@@ -35,8 +39,16 @@ class SettingsRepairStartButton extends StatelessWidget {
         RepairInitial() || RepairLoadOnFailure() => () =>
             context.read<RepairBloc>().add(const RepairStartEvent()),
         RepairLoadInProgress() || RepairAnimationInProgress() => null,
-        RepairLoadOnSuccess() => () => context.pop(),
+        RepairLoadOnSuccess() => _nativeApi.restartApp,
       };
+
+  void _onPop(BuildContext context) {
+    if (context.read<RepairBloc>().state is RepairLoadOnSuccess) {
+      unawaited(Injector.instance.nativeApi.restartApp());
+    }
+  }
+
+  static final _nativeApi = Injector.instance.nativeApi;
 
   TranslationsPagesSettingsRepairEn get _translations =>
       Injector.instance.translations.pages.settings.repair;
