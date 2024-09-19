89685c533565c9aff227242715962bcd23fa3d04
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-19 13:28:37 +0200

    Add error handling to MaterialThemeRepository

--- app/lib/domain/utils/mixins/logger.dart
+++ app/lib/domain/utils/mixins/logger.dart
@@ -3,53 +3,53 @@ import 'package:tft_guide/injector.dart';
 
 mixin LoggerMixin {
   void logInfo(
-    String name,
+    String methodName,
     String message, {
     required StackTrace stackTrace,
     Map<String, String?>? parameters,
   }) =>
       _loggerAPI.logInfo(
-        name,
+        methodName,
         message,
         parameters: parameters,
         stackTrace: stackTrace,
       );
 
   void logWarning(
-    String name,
+    String methodName,
     String message, {
     required StackTrace stackTrace,
     Map<String, String?>? parameters,
   }) =>
       _loggerAPI.logWarning(
-        name,
+        methodName,
         message,
         parameters: parameters,
         stackTrace: stackTrace,
       );
 
   void logException(
-    String name, {
+    String methodName, {
     required Exception exception,
     required StackTrace stackTrace,
     Map<String, String?>? parameters,
   }) =>
       _loggerAPI.logException(
-        name,
+        methodName,
         exception: exception,
         stackTrace: stackTrace,
         parameters: parameters,
       );
 
   void logOtherException(
-    String name, {
+    String methodName, {
     required Exception exception,
     required StackTrace stackTrace,
     Map<String, String?>? parameters,
   }) {
     if (exception is! CustomException) {
       logException(
-        name,
+        methodName,
         exception: exception,
         stackTrace: stackTrace,
         parameters: parameters,
