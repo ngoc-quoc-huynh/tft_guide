a2daffe92ffa4a723b9e90bd123ab818d0f2214e
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-09-26 11:01:44 +0200

    Add tests for PatchNote

---
+++ app/test/infrastructure/dtos/supabase/item_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('BaseItemMeta', () {
+    group('fromJson', () {
+      test('returns correctly.', () {});
+
+      test(
+        'throws TypeError if key is not found.',
+        () {},
+      );
+
+      test(
+        'throws TypeError if value is wrong type.',
+        () {},
+      );
+    });
+
+    group('toDomain', () {
+      test('returns correctly.', () {});
+    });
+  });
+}
