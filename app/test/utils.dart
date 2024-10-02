2b7a1eba620c99bdceb69686c64739f49588ca3f
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-10-02 12:03:32 +0200

    Fix computeThemeFromFileImage test

--- app/test/utils.dart
+++ app/test/utils.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:collection/collection.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:path/path.dart';
 
 extension DurationTestExtension on Duration {
   Duration get withThreshold => this + const Duration(milliseconds: 50);
@@ -9,3 +12,11 @@ void expectList<T>(List<T> actual, List<T> matcher) => expect(
       ListEquality<T>().equals(actual, matcher),
       isTrue,
     );
+
+final class TestFile {
+  const TestFile(this.path);
+
+  final String path;
+
+  File toFile() => File(join(Directory.current.path, 'test', path));
+}
