45bafb642ec4c120434a85e73a9d442b4aaa2948
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-10-11 11:16:48 +0200

    Add missing tests for AdminRepository

--- app/lib/infrastructure/repositories/admin.dart
+++ app/lib/infrastructure/repositories/admin.dart
@@ -1,10 +1,31 @@
 import 'package:tft_guide/domain/interfaces/admin.dart';
+import 'package:tft_guide/domain/utils/mixins/logger.dart';
 
-final class AdminRepository implements AdminApi {
+final class AdminRepository with LoggerMixin implements AdminApi {
   const AdminRepository(this._password);
 
   final String _password;
 
   @override
-  bool checkPassword(String password) => _password == password;
+  bool checkPassword(String password) {
+    const methodName = 'AdminRepository.checkPassword';
+    final parameters = {'password': password};
+
+    if (_password == password) {
+      logInfo(
+        methodName,
+        'Password is correct.',
+        parameters: parameters,
+        stackTrace: StackTrace.current,
+      );
+      return true;
+    }
+    logWarning(
+      methodName,
+      'Password is incorrect.',
+      parameters: parameters,
+      stackTrace: StackTrace.current,
+    );
+    return false;
+  }
 }
