2932e5739f6a60931b104dc2765cad87c995604c
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-11-04 13:18:35 +0100

    Add golden tests for ItemDetailPage

---
+++ app/test/ui/pages/item_detail/full_item_test.dart
@@ -0,0 +1,140 @@
+import 'package:alchemist/alchemist.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:file/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tft_guide/domain/blocs/value/cubit.dart';
+import 'package:tft_guide/domain/interfaces/file.dart';
+import 'package:tft_guide/domain/interfaces/local_database.dart';
+import 'package:tft_guide/domain/interfaces/theme.dart';
+import 'package:tft_guide/domain/models/database/language_code.dart';
+import 'package:tft_guide/domain/models/item_detail.dart';
+import 'package:tft_guide/injector.dart';
+import 'package:tft_guide/ui/pages/item_detail/full_item/page.dart';
+
+import '../../../mocks.dart';
+import '../../../utils.dart';
+
+Future<void> main() async {
+  final localDatabaseApi = MockLocalDatabaseApi();
+  final themeApi = MockThemeApi();
+  final fileStorageApi = MockFileStorageApi();
+
+  setUpAll(
+    () => Injector.instance
+      ..registerSingleton<Translations>(TranslationsEn.build())
+      ..registerSingleton<LocalDatabaseApi>(localDatabaseApi)
+      ..registerSingleton<ThemeApi>(themeApi)
+      ..registerSingleton<FileStorageApi>(fileStorageApi),
+  );
+
+  tearDownAll(
+    () async => Injector.instance
+      ..unregister<Translations>()
+      ..unregister<LocalDatabaseApi>()
+      ..unregister<ThemeApi>()
+      ..unregister<FileStorageApi>(),
+  );
+
+  final languageCodeValueCubit = MockLanguageCodeValueCubit();
+  whenListen<LanguageCode>(
+    languageCodeValueCubit,
+    const Stream<LanguageCode>.empty(),
+    initialState: LanguageCode.en,
+  );
+
+  final fileSystem = MemoryFileSystem();
+  when(() => fileStorageApi.loadFile(any()))
+      .thenReturn(fileSystem.file('test.webp'));
+
+  when(
+    () => localDatabaseApi.loadBaseItemDetail('base-item', LanguageCode.en),
+  ).thenAnswer(
+    (_) async => const BaseItemDetail(
+      id: 'base-item',
+      name: 'Name',
+      description: 'Description',
+      hint: 'Hint',
+    ),
+  );
+  const itemId = 'full-item';
+  when(() => localDatabaseApi.loadFullItemDetail(itemId, LanguageCode.en))
+      .thenAnswer(
+    (_) async => const FullItemDetail(
+      id: itemId,
+      name: 'Name',
+      description: 'Description',
+      hint: 'Hint',
+      itemId1: 'base-item',
+      itemId2: 'base-item',
+      abilityPower: 1,
+      armor: 2,
+      attackDamage: 3,
+    ),
+  );
+
+  await goldenTest(
+    'renders correctly.',
+    fileName: 'full_item',
+    pumpBeforeTest: (tester) async {
+      await precacheImages(tester);
+      await tester.pumpAndSettle();
+    },
+    constraints: pageConstraints(),
+    builder: () => _TestWidget(
+      languageCodeValueCubit: languageCodeValueCubit,
+      mockTheme: (context) => when(
+        () => themeApi.computeThemeFromFileImage(
+          fileName: itemId,
+          brightness: Theme.of(context).brightness,
+          textTheme: Theme.of(context).textTheme,
+        ),
+      ).thenAnswer((_) async => null),
+    ),
+  );
+
+  await goldenTest(
+    'renders theme correctly.',
+    fileName: 'full_item_theme',
+    pumpBeforeTest: (tester) async {
+      await precacheImages(tester);
+      await tester.pumpAndSettle();
+    },
+    constraints: pageConstraints(),
+    builder: () => _TestWidget(
+      languageCodeValueCubit: languageCodeValueCubit,
+      mockTheme: (context) => when(
+        () => themeApi.computeThemeFromFileImage(
+          fileName: itemId,
+          brightness: Theme.of(context).brightness,
+          textTheme: Theme.of(context).textTheme,
+        ),
+      ).thenAnswer(
+        (_) async => ThemeData.from(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        ),
+      ),
+    ),
+  );
+}
+
+final class _TestWidget extends StatelessWidget {
+  const _TestWidget({
+    required this.languageCodeValueCubit,
+    required this.mockTheme,
+  });
+
+  final LanguageCodeValueCubit languageCodeValueCubit;
+  final void Function(BuildContext) mockTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    mockTheme.call(context);
+    return BlocProvider<LanguageCodeValueCubit>.value(
+      value: languageCodeValueCubit,
+      child: FullItemDetailPage(id: 'full-item'),
+    );
+  }
+}
