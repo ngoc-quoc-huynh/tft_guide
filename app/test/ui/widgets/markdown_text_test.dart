6da8917b1764d8d1ac50c381bfa7bf5bd2dc4752
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2024-11-04 12:33:23 +0100

    Add golden tests for MarkdownText

---
+++ app/test/ui/widgets/markdown_text_test.dart
@@ -0,0 +1,18 @@
+import 'package:alchemist/alchemist.dart';
+import 'package:tft_guide/ui/widgets/markdown_text.dart';
+
+Future<void> main() => goldenTest(
+      'renders correctly',
+      fileName: 'markdown_text',
+      builder: () => GoldenTestScenario(
+        name: 'Default',
+        child: const MarkdownText(
+          text: '''
+# Heading 1
+## Heading 2
+**Bold text**
+Regular text.
+''',
+        ),
+      ),
+    );
